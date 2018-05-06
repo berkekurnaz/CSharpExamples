48c813a1c773c51068daf2a0c84e5284709fbd1d
BerkeKurnaz <34648016+berkekurnaz@users.noreply.github.com>
2018-05-06 16:13:03 +0300

    Add files via upload

---
+++ SezarSifrelemeYontemi/SezarSifrelemeYontemi/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace SezarSifrelemeYontemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnKelimeSifrele = new System.Windows.Forms.Button();
+            this.TxtSifrelenmisKelime = new System.Windows.Forms.TextBox();
+            this.TxtSifrelenecekKelime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnSifreCoz = new System.Windows.Forms.Button();
+            this.TxtCozulmusKelime = new System.Windows.Forms.TextBox();
+            this.TxtSifrelenmisKelime2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.BtnKelimeSifrele);
+            this.groupBox1.Controls.Add(this.TxtSifrelenmisKelime);
+            this.groupBox1.Controls.Add(this.TxtSifrelenecekKelime);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 344);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kelimeyi Şifrele";
+            // 
+            // BtnKelimeSifrele
+            // 
+            this.BtnKelimeSifrele.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKelimeSifrele.Location = new System.Drawing.Point(80, 266);
+            this.BtnKelimeSifrele.Name = "BtnKelimeSifrele";
+            this.BtnKelimeSifrele.Size = new System.Drawing.Size(291, 63);
+            this.BtnKelimeSifrele.TabIndex = 4;
+            this.BtnKelimeSifrele.Text = "KELİME ŞİFRELE";
+            this.BtnKelimeSifrele.UseVisualStyleBackColor = true;
+            this.BtnKelimeSifrele.Click += new System.EventHandler(this.BtnKelimeSifrele_Click);
+            // 
+            // TxtSifrelenmisKelime
+            // 
+            this.TxtSifrelenmisKelime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSifrelenmisKelime.Location = new System.Drawing.Point(200, 113);
+            this.TxtSifrelenmisKelime.Multiline = true;
+            this.TxtSifrelenmisKelime.Name = "TxtSifrelenmisKelime";
+            this.TxtSifrelenmisKelime.ReadOnly = true;
+            this.TxtSifrelenmisKelime.Size = new System.Drawing.Size(248, 135);
+            this.TxtSifrelenmisKelime.TabIndex = 3;
+            // 
+            // TxtSifrelenecekKelime
+            // 
+            this.TxtSifrelenecekKelime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSifrelenecekKelime.Location = new System.Drawing.Point(200, 71);
+            this.TxtSifrelenecekKelime.Name = "TxtSifrelenecekKelime";
+            this.TxtSifrelenecekKelime.Size = new System.Drawing.Size(248, 30);
+            this.TxtSifrelenecekKelime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(18, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifrelenmiş Kelime :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(6, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifrelenecek Kelime :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox2.Controls.Add(this.BtnSifreCoz);
+            this.groupBox2.Controls.Add(this.TxtCozulmusKelime);
+            this.groupBox2.Controls.Add(this.TxtSifrelenmisKelime2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(512, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(467, 344);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Şifreyi Çöz";
+            // 
+            // BtnSifreCoz
+            // 
+            this.BtnSifreCoz.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSifreCoz.Location = new System.Drawing.Point(80, 266);
+            this.BtnSifreCoz.Name = "BtnSifreCoz";
+            this.BtnSifreCoz.Size = new System.Drawing.Size(291, 63);
+            this.BtnSifreCoz.TabIndex = 4;
+            this.BtnSifreCoz.Text = "ŞİFREYİ ÇÖZ";
+            this.BtnSifreCoz.UseVisualStyleBackColor = true;
+            this.BtnSifreCoz.Click += new System.EventHandler(this.BtnSifreCoz_Click);
+            // 
+            // TxtCozulmusKelime
+            // 
+            this.TxtCozulmusKelime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtCozulmusKelime.Location = new System.Drawing.Point(200, 113);
+            this.TxtCozulmusKelime.Multiline = true;
+            this.TxtCozulmusKelime.Name = "TxtCozulmusKelime";
+            this.TxtCozulmusKelime.ReadOnly = true;
+            this.TxtCozulmusKelime.Size = new System.Drawing.Size(248, 135);
+            this.TxtCozulmusKelime.TabIndex = 3;
+            // 
+            // TxtSifrelenmisKelime2
+            // 
+            this.TxtSifrelenmisKelime2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSifrelenmisKelime2.Location = new System.Drawing.Point(200, 71);
+            this.TxtSifrelenmisKelime2.Name = "TxtSifrelenmisKelime2";
+            this.TxtSifrelenmisKelime2.Size = new System.Drawing.Size(248, 30);
+            this.TxtSifrelenmisKelime2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(18, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Çözülmüş Kelime :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(10, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Şifrelenmiş Kelime :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(297, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(409, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SEZAR ŞİFRELEME YÖNTEMİ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.LemonChiffon;
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(13, 514);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(966, 122);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sezar Şifreleme Yöntemi Nedir ?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(16, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(931, 77);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 660);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Sezar Sifreleme Yontemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnKelimeSifrele;
+        private System.Windows.Forms.TextBox TxtSifrelenmisKelime;
+        private System.Windows.Forms.TextBox TxtSifrelenecekKelime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnSifreCoz;
+        private System.Windows.Forms.TextBox TxtCozulmusKelime;
+        private System.Windows.Forms.TextBox TxtSifrelenmisKelime2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SezarSifrelemeYontemi/SezarSifrelemeYontemi/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SezarSifrelemeYontemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /* Kelime Sifrele Butonu Tiklama Olaylari */
+        private void BtnKelimeSifrele_Click(object sender, EventArgs e)
+        {
+            if (TxtSifrelenecekKelime.Text == "")
+            {
+                MessageBox.Show("Lutfen Bos Alanlari Doldurunuz...", "Bilgi Kutusu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                TxtSifrelenmisKelime.Text = "";
+                string kelime = "";
+                kelime = TxtSifrelenecekKelime.Text;
+                char[] kelimeDizisi = kelime.ToCharArray();
+                foreach (char kelimeler in kelimeDizisi)
+                {
+                    TxtSifrelenmisKelime.Text += Convert.ToChar(kelimeler + 3).ToString();
+                }
+            }
+        }
+
+
+        /* Sifre Coz Butonu Tiklama Olaylari */
+        private void BtnSifreCoz_Click(object sender, EventArgs e)
+        {
+            if (TxtSifrelenmisKelime2.Text == "")
+            {
+                MessageBox.Show("Lutfen Bos Alanlari Doldurunuz...", "Bilgi Kutusu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                TxtCozulmusKelime.Text = "";
+                string sifre = "";
+                sifre = TxtSifrelenmisKelime2.Text;
+                char[] sifreDizisi = sifre.ToCharArray();
+                foreach (char sifreler in sifreDizisi)
+                {
+                    TxtCozulmusKelime.Text += Convert.ToChar(sifreler - 3).ToString();
+                }
+            }
+        }
+
+    }
+}
